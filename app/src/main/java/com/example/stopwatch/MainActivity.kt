d7c75c8610126f5ef7ffef20af7dd2138b9362fe
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-09-04 11:05:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stopwatch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var isStarted = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener{
+            startOrStop()
+        }
+        binding.btnReset.setOnClickListener{
+            reset()
+        }
+    }
+
+    private fun startOrStop(){
+        if (isStarted)
+            stop()
+        else
+            start()
+    }
+
+    private fun start(){
+        binding.btnStart.text = "Stop"
+        isStarted = true
+    }
+
+    private fun stop(){
+        binding.btnStart.text = "Start"
+        isStarted = false
+    }
+
+    private fun reset(){
+
+    }
+}
